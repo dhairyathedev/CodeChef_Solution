0947f931953e54e0e7d80a4d25c8a34db9f748fe
Dhairya Shah <dtshah24022006@gmail.com>
2023-07-18 19:33:54 +0530

    Create squats.cpp

---
+++ squats.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin >> x;
+	while(x--){
+	    int x;
+	    cin >> x;
+	    cout << x * 15 << endl;
+	}
+	return 0;
+}
